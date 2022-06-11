42f491d10c668f0313f3d97156606f4d28c25308
Caro <paulacarolinaserra@gmail.com>
2022-06-11 20:34:49 -0300

    Home & Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,48 @@ import { Link } from "react-router-dom"
 
 function Header() {
   return (
-    <header>
-        <img
-            alt="logo"
-            src="/assets/images/logo-moviefan.png"
-            width="500"
-            className="d-inline-block align-top"
-        />
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">HOME</Link>
-                </li>
-                <li>
-                </li>
-            </ul>
-        </nav>
+    <header className="container-fluid">
+        <div className="row">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <img
+                    alt="logo"
+                    src="/assets/images/logo-moviefan.png"
+                    width="400"
+                    className="d-inline-block align-top"
+                />
+                <div className="col-6 navbar-collapse" >
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="#">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="#">PELICULAS</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="#">PELICULAS</Link>
+                        </li>
+                    </ul>
+                </div>
+                <form className="form-inline my-2 my-lg-0 mr-1">
+                    <input className="form-control mr-sm-1" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success my-1 my-sm-0" type="submit">Search</button>
+                 </form>
+                 <div className="col-1 navbar-collapse" >
+                        <div className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                ME
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <Link className="dropdown-item" to="#">Mis datos</Link>
+                                <Link className="dropdown-item" to="#">Mis favoritos</Link>
+                                <div className="dropdown-divider" />
+                                <Link className="dropdown-item" to="#">Configuracion</Link>
+                            </div>
+                        </div>
+                </div>
+            </nav>
+        </div>
+                
     </header>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,52 +1,51 @@
+import { useEffect, useState } from "react";
 import { Link, Navigate } from "react-router-dom"
+import swAlert from '@sweetalert/with-react';
+import axios from "axios";
 import Header from "./Header";
 import Footer from "./Footer";
 
 function Home() {
     let token = localStorage.getItem('token');
 
+    const [movieList, setMovieList] = useState([]);
+    console.log(movieList)
+
+    useEffect(() => {
+        const endPoint = 'https://api.themoviedb.org/3/discover/movie?api_key=ff5a58eee969df72fea087a6c2d04059'
+        axios.get(endPoint)
+            .then(res => {
+                const apiData = res.data
+                setMovieList(apiData.results)
+            })
+            .catch(err => {
+                swAlert(<h2>Se produjo un error. Reintente mas tarde.</h2>)
+            })
+    }, [setMovieList]);
+
     return (
         <>
-        {!token && <Navigate to="/" />}
-        <Header /> 
-        <div className="card-group">
-            <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">movie title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <Link to="" className="btn btn-primary">Ver pelicula</Link>
-                </div>
-            </div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">movie title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <Link to="" className="btn btn-info">Ver pelicula</Link>
-                </div>
-            </div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">movie title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <Link to="" className="btn btn-primary">Ver pelicula</Link>
-                </div>
-            </div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">movie title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <Link to="" className="btn btn-primary">Ver pelicula</Link>
-                </div>
-            </div>
-        </div>
+            { !token && <Navigate to="/" /> }
 
-        <Footer />
-        </>
-            
+            <Header /> 
+            <div className="row col-12 justify-content-around">
+                {
+                    movieList.map((oneMovie, idx) => {
+                        return (
+                            <div className="card col-3 mt-5 m-1 p-1" key={idx} style={{width: "20rem"}} >
+                                <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">{oneMovie.title}</h5>
+                                    <p className="card-text">{oneMovie.overview.substring(0,200)}. <Link to="" >Ver mas...</Link></p>
+                                    <Link to={`/detail?movieID=${oneMovie.id}`} className="btn btn-primary">Ver pelicula</Link>
+                                </div>
+                            </div>
+                        )
+                    })
+                }          
+            </div>
+            <Footer />
+        </>            
     )
 }
 
